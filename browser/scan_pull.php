4fc853c51dc8340342062336706e720ede6f8814
formywor <nidalwonderofficialyt@gmail.com>
2025-10-09 19:59:56 -0400

    Update scan_pull.php

--- browser/scan_pull.php
+++ browser/scan_pull.php
@@ -6,17 +6,10 @@ header('Pragma: no-cache');
 $session = isset($_GET['session']) ? preg_replace('/[^A-Za-z0-9\-_.]/','', $_GET['session']) : '';
 if ($session === '') { exit(''); }
 
-// Attach to the same session id the HTA created
 session_id($session);
 session_start();
-
-$digits = '';
-if (!empty($_SESSION['code'])) {
-  $digits = preg_replace('/\D/', '', (string)$_SESSION['code']);
-  // one-time read
-  $_SESSION['code'] = '';
-}
+$digits = isset($_SESSION['code']) ? preg_replace('/\D/','', (string)$_SESSION['code']) : '';
+$_SESSION['code'] = '';  // one-time read
 session_write_close();
 
-// Return digits (or empty if not set yet)
 echo $digits;
